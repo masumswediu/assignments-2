af546341755b0c5376d23336fdf370c07d40f52d
masum <masfikur35-3118@diu.edu.bd>
2022-03-25 02:36:52 +0600

    assignment class 02

---
+++ assignment2.php
@@ -0,0 +1,49 @@
+<!-- Case Study:
+
+	I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a
+     product upload form on my website, then upload my product 
+    with specification and available quantity. My monitors have a 
+    15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+
+For creating a product upload form what specifications will be uploaded and what will be their data types?
+
+Directions:
+
+    N.B: Approximately 6 Variable will be there
+
+    Create a php file.
+
+    Comment this case study on this php file. 
+
+    Then write down these variable names with value by following the variable's declaration rules.
+
+    And write the data type of every variable's right side as comment code.(ex: $age = 10 // data type is double)
+
+    Create a Project on github and upload this assignment and give this project link on assignment.
+
+    An example has been attached to this attachment section. -->
+
+<?php
+
+$product_neme = "Monitor";// Data type string
+$product_quantity = 3 ; // data type Integer
+$monitor_size = 15.6;//data type double/float
+$total_amount = 8000; //data type integer
+$vga_status = true;// data type boolean
+$product_specification = Null;// data type null
+
+
+
+// echo gettype($product_neme);
+// echo gettype($monitor_size);
+// echo gettype($monitor_size);
+// echo gettype($total_amount);
+// echo gettype($vga_status);
+echo gettype($product_specification);
+
+
+
+
+
+
+?>
